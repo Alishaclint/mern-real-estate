9c7cadc9b6821806e2e94d1b86b96d6ec1e330af
Alishaclint <annaalishadarly@gmail.com>
2024-02-20 10:28:08 +0530

    added header component

--- client-front-end/src/App.jsx
+++ client-front-end/src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import Header from "./components/Header"
+
+
 
 const App = () => {
   return (
-    <div className="text-blue-500">
-      <h1>Hello</h1>
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/>} />
 
-    </div>
+      </Routes>
+      </BrowserRouter>
+    
+    
+    </>
+
+   
   )
 }
 
